ae4c521ee39461000a2a0cd3061e568ebdceb3c1
Yoann Jordany <yoann.jordany@gmail.com>
2024-08-01 23:04:22 +0200

    Fix JWT_SECRET

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,133 +1,136 @@
-import { Injectable } from '@nestjs/common';
-import { MemberService } from 'src/member/member.service';
-import { Member } from '@prisma/client';
-import { JwtService } from '@nestjs/jwt';
+import {Injectable} from '@nestjs/common';
+import {MemberService} from 'src/member/member.service';
+import {Member} from '@prisma/client';
+import {JwtService} from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 
 type Login = {
-  accessToken: string;
-  refreshToken: string;
+    accessToken: string;
+    refreshToken: string;
 };
 
 type Payload = {
-  email: string;
-  id: number;
-  role: string;
+    email: string;
+    id: number;
+    role: string;
 };
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private memberService: MemberService,
-    private jwtService: JwtService,
-  ) {}
-
-  async validateMember(email: string, pass: string): Promise<Member | null> {
-    const member = await this.memberService.member({ email });
-    if (!member) {
-      return null;
-    }
-    const isPasswordValid = await bcrypt.compare(pass, member.password);
-    if (isPasswordValid) {
-      return member;
-    }
-    return null;
-  }
-
-  async login(member: Member, res) {
-    const payload = this.getMemberPayload(member);
-
-    const refreshToken = await this.getNewRefreshToken(member, payload);
-
-    const login = {
-      accessToken: this.getNewAccessToken(payload),
-      refreshToken,
-    };
-
-    this.sendLoginResponse(login, res, member);
-  }
-
-  async refresh(refreshToken: string, res: Response) {
-    const payload = this.jwtService.verify(refreshToken);
-    const member = await this.memberService.member({
-      id: payload.id,
-    });
-
-    if (!member) {
-      throw new Error('Member not found');
+    constructor(
+        private memberService: MemberService,
+        private jwtService: JwtService,
+    ) {
     }
 
-    const isRefreshTokenValid = bcrypt.compare(
-      refreshToken,
-      member.refreshToken,
-    );
-
-    if (!isRefreshTokenValid) {
-      throw new Error('Refresh token is not valid');
+    async validateMember(email: string, pass: string): Promise<Member | null> {
+        const member = await this.memberService.member({email});
+        if (!member) {
+            return null;
+        }
+        const isPasswordValid = await bcrypt.compare(pass, member.password);
+        if (isPasswordValid) {
+            return member;
+        }
+        return null;
     }
 
-    const newPayload = this.getMemberPayload(member);
-    const newRefreshToken = await this.getNewRefreshToken(member, newPayload);
+    async login(member: Member, res) {
+        const payload = this.getMemberPayload(member);
 
-    const login = {
-      accessToken: this.getNewAccessToken(newPayload),
-      refreshToken: newRefreshToken,
-    };
+        const refreshToken = await this.getNewRefreshToken(member, payload);
 
-    this.sendLoginResponse(login, res, null);
-  }
+        const login = {
+            accessToken: this.getNewAccessToken(payload),
+            refreshToken,
+        };
 
-  async loginMember(member: Member, res) {
-    const payload = this.getMemberPayload(member);
-    const refreshToken = await this.getNewRefreshToken(member, payload);
+        this.sendLoginResponse(login, res, member);
+    }
 
-    const login = {
-      accessToken: this.getNewAccessToken(payload),
-      refreshToken,
-    };
+    async refresh(refreshToken: string, res: Response) {
+        const payload = this.jwtService.verify(refreshToken);
+        const member = await this.memberService.member({
+            id: payload.id,
+        });
 
-    this.sendLoginResponse(login, res, member);
-  }
+        if (!member) {
+            throw new Error('Member not found');
+        }
 
-  getNewAccessToken(payload: Payload) {
-    return this.jwtService.sign(payload, {
-      expiresIn: '15m',
-    });
-  }
+        const isRefreshTokenValid = bcrypt.compare(
+            refreshToken,
+            member.refreshToken,
+        );
 
-  async getNewRefreshToken(member: Member, payload: Payload) {
-    const refreshToken = this.jwtService.sign(payload, {
-      expiresIn: '30d',
-    });
+        if (!isRefreshTokenValid) {
+            throw new Error('Refresh token is not valid');
+        }
 
-    const refreshTokenHash = await bcrypt.hash(refreshToken, 10);
-    await this.memberService.updateRefreshToken(
-      member.id.toString(),
-      refreshTokenHash,
-    );
+        const newPayload = this.getMemberPayload(member);
+        const newRefreshToken = await this.getNewRefreshToken(member, newPayload);
 
-    return refreshToken;
-  }
+        const login = {
+            accessToken: this.getNewAccessToken(newPayload),
+            refreshToken: newRefreshToken,
+        };
 
-  getMemberPayload(member: Member) {
-    return {
-      email: member.email,
-      id: member.id,
-      role: member.role,
-    };
-  }
+        this.sendLoginResponse(login, res, null);
+    }
 
-  //TODO: Hide crypted refresh token in member (look at DTO ?)
-  sendLoginResponse(login: Login, res, member) {
-    res.cookie('refresh_token', login.refreshToken, {
-      httpOnly: true,
-      secure: process.env.ENV !== 'local',
-      sameSite: 'strict',
-    });
+    async loginMember(member: Member, res) {
+        const payload = this.getMemberPayload(member);
+        const refreshToken = await this.getNewRefreshToken(member, payload);
 
-    res.header('X-Access-token', login.accessToken);
-    res.json({
-      ...member,
-    });
-  }
+        const login = {
+            accessToken: this.getNewAccessToken(payload),
+            refreshToken,
+        };
+
+        this.sendLoginResponse(login, res, member);
+    }
+
+    getNewAccessToken(payload: Payload) {
+        return this.jwtService.sign(payload, {
+            expiresIn: '15m',
+            secret: process.env.JWT_SECRET,
+        });
+    }
+
+    async getNewRefreshToken(member: Member, payload: Payload) {
+        const refreshToken = this.jwtService.sign(payload, {
+            expiresIn: '30d',
+            secret: process.env.JWT_SECRET,
+        });
+
+        const refreshTokenHash = await bcrypt.hash(refreshToken, 10);
+        await this.memberService.updateRefreshToken(
+            member.id.toString(),
+            refreshTokenHash,
+        );
+
+        return refreshToken;
+    }
+
+    getMemberPayload(member: Member) {
+        return {
+            email: member.email,
+            id: member.id,
+            role: member.role,
+        };
+    }
+
+    //TODO: Hide crypted refresh token in member (look at DTO ?)
+    sendLoginResponse(login: Login, res, member) {
+        res.cookie('refresh_token', login.refreshToken, {
+            httpOnly: true,
+            secure: process.env.ENV !== 'local',
+            sameSite: 'strict',
+        });
+
+        res.header('X-Access-token', login.accessToken);
+        res.json({
+            ...member,
+        });
+    }
 }
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,36 +1,36 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { patchNestJsSwagger } from 'nestjs-zod';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
+import {patchNestJsSwagger} from 'nestjs-zod';
 import * as cookieParser from 'cookie-parser';
 
 patchNestJsSwagger();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
+    const app = await NestFactory.create(AppModule);
+    app.use(cookieParser());
 
-  app.enableCors({
-    origin: process.env.FRONT_URL,
-    exposedHeaders: ['X-Access-Token', 'Set-Cookie', 'X-Total-Count'],
-    credentials: true,
-  });
+    app.enableCors({
+        origin: process.env.FRONT_URL,
+        exposedHeaders: ['X-Access-Token', 'Set-Cookie', 'X-Total-Count'],
+        credentials: true,
+    });
 
-  const config = new DocumentBuilder()
-    .setTitle('Anansi Forum API')
-    .setDescription('The Anansi Forum API description')
-    .addBearerAuth()
-    .setVersion('1.0')
-    .build();
+    const config = new DocumentBuilder()
+        .setTitle('Anansi Forum API')
+        .setDescription('The Anansi Forum API description')
+        .addBearerAuth()
+        .setVersion('1.0')
+        .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
-    swaggerOptions: {
-      persistAuthorization: true,
-    },
-  });
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document, {
+        swaggerOptions: {
+            persistAuthorization: true,
+        },
+    });
 
-  await app.listen(process.env.LISTENING_PORT);
+    await app.listen(process.env.LISTENING_PORT);
 }
 
 bootstrap();
